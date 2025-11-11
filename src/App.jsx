a92146ea3c31744804105386b67fc6e7435e569f
your-github-username <your-github-email>
2025-11-11 08:57:30 +0700

    intailize

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ const App = () => {
     <div className="flex flex-col min-h-screen">
       {/* Header stays on top */}
       <Header />
-
+FUCK YOU
       {/* Main content grows to fill space */}
       <main className="flex-grow">
         <AppRoutes />
